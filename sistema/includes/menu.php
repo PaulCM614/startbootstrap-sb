a3a27e823dcd69562db2f9328f09f6baeaf30a1e
PaulCM614 <paulgeovannicharcamamani@gmail.com>
2024-07-28 19:46:53 -0500

    Add files via upload

---
+++ sistema/includes/menu.php
@@ -0,0 +1,93 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+	<!-- Sidebar - Brand -->
+	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+		<div class="sidebar-brand-icon rotate-n-15">
+			<img src="img/fac.png" class="img-thumbnail">
+		</div>
+		<div class="sidebar-brand-text mx-3">SISTEMA FACTURACION</div>
+	</a>
+
+	<!-- Divider -->
+	<hr class="sidebar-divider my-0">
+
+	<!-- Divider -->
+	<hr class="sidebar-divider">
+
+	<!-- Heading -->
+	<div class="sidebar-heading">
+		Interface
+	</div>
+
+	<!-- Nav Item - Pages Collapse Menu -->
+	<li class="nav-item">
+		<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+			<i class="fas fa-fw fa-cog"></i>
+			<span>Ventas</span>
+		</a>
+		<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+			<div class="bg-white py-2 collapse-inner rounded">
+				<a class="collapse-item" href="nueva_venta.php">AGREGAR NUEVA VENTA</a>
+				<a class="collapse-item" href="ventas.php">VER VENTAS</a>
+			</div>
+		</div>
+	</li>
+
+	<!-- Nav Item - Productos Collapse Menu -->
+	<li class="nav-item">
+		<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+			<i class="fas fa-fw fa-wrench"></i>
+			<span>Productos</span>
+		</a>
+		<div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+			<div class="bg-white py-2 collapse-inner rounded">
+				<a class="collapse-item" href="registro_producto.php">AGREGAR NUEVO PRODUCTO</a>
+				<a class="collapse-item" href="lista_productos.php">PRODUCTOS</a>
+			</div>
+		</div>
+	</li>
+
+	<!-- Nav Item - Clientes Collapse Menu -->
+	<li class="nav-item">
+		<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseClientes" aria-expanded="true" aria-controls="collapseUtilities">
+			<i class="fas fa-users"></i>
+			<span>Clientes</span>
+		</a>
+		<div id="collapseClientes" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+			<div class="bg-white py-2 collapse-inner rounded">
+				<a class="collapse-item" href="registro_cliente.php">AGREGAR NUEVO CLIENTE</a>
+				<a class="collapse-item" href="lista_cliente.php">VER CLIENTES</a>
+			</div>
+		</div>
+	</li>
+	<!-- Nav Item - Utilities Collapse Menu -->
+	<li class="nav-item">
+		<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseProveedor" aria-expanded="true" aria-controls="collapseUtilities">
+			<i class="fas fa-hospital"></i>
+			<span>Proveedor</span>
+		</a>
+		<div id="collapseProveedor" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+			<div class="bg-white py-2 collapse-inner rounded">
+				<a class="collapse-item" href="registro_proveedor.php">AGREGAR NUEVO PROVEEDOR</a>
+				<a class="collapse-item" href="lista_proveedor.php">VER PROVEEDORES</a>
+			</div>
+		</div>
+	</li>
+	<?php if ($_SESSION['rol'] == 1) { ?>
+		<!-- Nav Item - Usuarios Collapse Menu -->
+		<li class="nav-item">
+			<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUsuarios" aria-expanded="true" aria-controls="collapseUtilities">
+				<i class="fas fa-user"></i>
+				<span>Usuarios</span>
+			</a>
+			<div id="collapseUsuarios" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+				<div class="bg-white py-2 collapse-inner rounded">
+					<a class="collapse-item" href="registro_usuario.php">AGREGAR NUEVO USUARIO</a>
+					<a class="collapse-item" href="lista_usuarios.php">VER USUARIOS</a>
+				</div>
+			</div>
+		</li>
+	<?php } ?>
+
+</ul>
